5fe0a0e0be4bb003ccc9adec441382b2f2e97bb9
Alejandra Ruiz Meza <alejandraruizmeza@Alejandras-MacBook-Air.local>
2023-09-29 21:52:23 +0530

    adding SEO to the LOCAL SEO

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from 'react';
 
-export default function SEO() {
+export function SEO ({
+  title,
+  description,
+  keywords,
+  author,
+  image,
+  url,
+  businessName,
+  address,
+  phone,
+  geo,
+  openingHours,
+  socialLinks,
+}) {
   return (
     <>
-        <meta  />
+      {/* Meta Tags */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="author" content={author} />
+
+      {/* Open Graph (OG) Meta Tags for Social Media */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:url" content={url} />
+
+      {/* JSON-LD Schema Markup for Local Business */}
+      <script type="application/ld+json">
+        {{
+          "@context": "http://schema.org",
+          "@type": "LocalBusiness",
+          "name": businessName,
+          "address": {
+            "@type": "PostalAddress",
+            "streetAddress": address.streetAddress,
+            "addressLocality": address.addressLocality,
+            "addressRegion": address.addressRegion,
+            "postalCode": address.postalCode,
+            "addressCountry": address.addressCountry
+          },
+          "telephone": phone,
+          "url": url,
+          "image": image,
+          "description": description,
+          "geo": {
+            "@type": "GeoCoordinates",
+            "latitude": geo.latitude,
+            "longitude": geo.longitude
+          },
+          "openingHours": openingHours,
+          "sameAs": socialLinks
+        }}
+      </script>
     </>
-  )
-}
+  );
+};
+
+
