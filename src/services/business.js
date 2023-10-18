1082933b1ea72600f997ded51ee8cf97f39fb747
Sebastian Septien <sebsepdus@hotmail.com>
2023-10-17 18:42:41 -0600

    Adding the json format so it could have its own population

--- src/services/business.js
+++ src/services/business.js
@@ -1,52 +1,78 @@
 import { TrophyIcon } from '@heroicons/react/20/solid'
 
 export const business = {
-  title: '',
-  description: '',
-  keywords: '',
-  author: '',
-  email: '',
+  title: 'Latido',
+  description:
+    '¡Bienvenido a Latido! Somos tu tienda de accesorios para mujer con precios competitivos. Explora nuestra amplia gama de bisutería y maquillaje, incluyendo esmaltes, aretes, collares y chapa de oro. Trabajamos con marcas reconocidas como Prosa, Bissu y Saniye, garantizando calidad y estilo en cada pieza. Ya sea para tu look diario o una ocasión especial, encuentra el complemento perfecto en Latido y realza tu estilo con nuestras opciones irresistibles. Latido te ofrece la combinación perfecta de moda y asequibilidad. Nuestra colección está cuidadosamente seleccionada para brindarte variedad y calidad en cada elección. Desde esmaltes vibrantes hasta maquillaje de tendencia, nuestros productos resaltarán tu belleza única',
+  keywords:
+    'accesorios, mujer, bisutería, maquillaje, esmaltes, aretes, collares, chapa de oro, Saniye, Prosa, Bissu',
+  author: 'Latido - Accesorios para Mujer',
+  email: 'arum_15@gmail.com',
   image: '',
   url: '',
-  businessName: '',
+  businessName: 'Latido',
   address: {
-    streetAddress: '',
-    addressLocality: '',
-    addressRegion: '',
-    postalCode: ' ',
-    addressCountry: '',
+    streetAddress: 'Av Cerro Sombrerete 1199',
+    addressLocality: 'Misión Bucareli Sur',
+    addressRegion: 'Santiago de Querétaro',
+    postalCode: '76168',
+    addressCountry: 'México',
   },
-  phone: '',
+  phone: '4626059572',
   geo: {
-    latitude: '',
-    longitude: '',
+    latitude: '20.623990',
+    longitude: '-100.412060',
   },
-  openingHours: '',
+  openingHours: "",
   socialLinks: [],
   testimonials: [
     {
-      body: '',
-      rating: '',
+      body: 'Excelente atención',
+      rating: 5,
       author: {
-        name: '',
+        name: 'Carmen Cruz Servin',
+      },
+    },
+    {
+      body: '',
+      rating: 5,
+      author: {
+        name: 'Alejandra Ruiz',
+      },
+    },
+    {
+      body: 'Buenos precios y buenos accesorios para el regalo de mi esposa!',
+      rating: 5,
+      author: {
+        name: 'Jorge Asdrubal',
+      },
+    },
+    {
+      body: 'Excelentes productos, me encanta todo',
+      rating: 5,
+      author: {
+        name: 'Alexa Guerrero',
       },
     },
   ],
   features: [
     {
-      name: '',
-      description: '',
-      icon: TrophyIcon,
+      name: 'Cosméticos',
+      description:
+        'Ofrecemos una grande gama de marcas de cosméticos y maquillaje como Saniye, Prossa, Bissu y entre otras marcas.',
+      icon: 'TrophyIcon',
     },
     {
-      name: '',
-      description: '',
-      icon: TrophyIcon,
+      name: 'Bisutería',
+      description:
+        'Contamos con una amplia gama de aretes, collares, lentes, moños a precios muy accesibles.',
+      icon: 'TrophyIcon',
     },
     {
-      name: '',
-      description: '',
-      icon: TrophyIcon,
+      name: 'Chapa de Oro',
+      description:
+        'Tenemos una variedad de aretes y anillos en chapa de oro a precios bastante económicos.',
+      icon: 'TrophyIcon',
     },
   ],
   images: [
@@ -57,11 +83,32 @@ export const business = {
   ],
   faqs: [
     {
-      question: '',
-      answer: '',
+      question: '¿Manejan marcas originales?',
+      answer:
+        'Sí, en Latido manejamos marcas reconocidas y originales como Saniye, Prossa, Bissu, entre otras. Garantizamos calidad y autenticidad en cada producto.',
+    },
+    {
+      question: '¿Realizan envíos a domicilio?',
+      answer:
+        'Actualmente, ofrecemos un servicio de compra en tienda. Sin embargo, estamos trabajando para implementar envíos a domicilio en el futuro cercano.',
+    },
+    {
+      question: '¿Ofrecen descuentos o promociones?',
+      answer:
+        '¡Sí! Con regularidad tenemos promociones y descuentos especiales. Te recomendamos seguirnos en nuestras redes sociales o suscribirte a nuestro boletín para estar al tanto de todas nuestras ofertas.',
+    },
+    {
+      question: '¿Tienen opciones de maquillaje hipoalergénico?',
+      answer:
+        'Sí, contamos con una gama de productos hipoalergénicos pensados para pieles sensibles. Puedes preguntar en tienda para que nuestros asesores te guíen hacia las mejores opciones según tus necesidades.',
+    },
+    {
+      question: '¿Cuál es el horario de atención de la tienda?',
+      answer:
+        'Nuestro horario de atención es todos los días de 10:00 am a 8:30 pm. Siempre estamos encantados de atenderte y ayudarte a encontrar el producto perfecto.',
     },
   ],
-  placeId: '',
+  placeId: 'ChIJ_QjHL99b04URen0FHTq22aM',
 }
 
 /*
