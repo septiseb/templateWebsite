6d80a3bd16d4affd0e1d77a6c0a3ca0a378fa17e
Sebastian Septien <sebsepdus@hotmail.com>
2023-10-18 19:42:32 -0600

    Adding the content tu unity website

--- src/services/business.js
+++ src/services/business.js
@@ -1,67 +1,113 @@
 import { TrophyIcon } from '@heroicons/react/20/solid'
 
 export const business = {
-  title: '',
-  description: '',
-  keywords: '',
-  author: '',
-  email: '',
-  image: '',
-  url: '',
-  businessName: '',
-  address: {
-    streetAddress: '',
-    addressLocality: '',
-    addressRegion: '',
-    postalCode: ' ',
-    addressCountry: '',
+  title: 'UNITY Fisioterapia',
+description: 'Somos la fusión de la fisioterapia y el entrenamiento físico, creemos que es la sinergia perfecta para un buen desempeño deportivo así como en actividades básicas de la vida diaria. Encontraras el balance perfecto entre bienestar físico y los increíbles beneficios de hacer ejercicio. La terapia física se ha convertido en parte fundamental tanto en procesos de rehabilitación como en tratamientos preventivos.',
+keywords: 'Fisioterapia, Entrenamiento terapéutico, Recuperación óptima',
+author: 'UNITY',
+email: 'fisioterapiaunity@gmail.com',
+image: 'https://storage.googleapis.com/web-assets-fluxsales/unityfisiowebsite/WhatsApp%20Image%202023-10-18%20at%205.34.13%20PM%20(2).jpeg',
+url: '',
+businessName: 'UNITY Fisioterapia',
+address: {
+  streetAddress: 'Plaza Distrito Marques, Av Paseo de la Reforma 231-Local 216, El Campanario, Lomas del Marques',
+  addressLocality: 'Querétaro',
+  addressRegion: 'Qro',
+  postalCode: '76146',
+  addressCountry: 'Mexico',
+},
+phone: '4423564055',
+geo: {
+  latitude: '20.612965011916376',
+  longitude: '-100.36088899386867',
+},
+openingHours: 'Mo,Tu,We,Th 09:00-13:00, 16:00-20:00; Fr,Sa 09:00-13:00; Su Closed',
+socialLinks: [ 
+  'https://www.instagram.com/unity_fisioterapia?igshid=MWZjMTM2ODFkZg%3D%3D',
+  ],
+testimonials: [{
+  body: 'Una excelente clínica que cuenta con profesionales muy bien preparados en las mejores instalaciones',
+  rating: 5,
+  author: {
+    name: 'Daniella Padilla',
   },
-  phone: '',
-  geo: {
-    latitude: '',
-    longitude: '',
+   },{
+  body: 'Excelente atención y servicio!! El mejor lugar para fisioterapia en Queretaro!',
+  rating: 5,
+  author: {
+    name: 'Ximena Frutis'},
+},{
+  body: 'Excelentes instalaciones y el personal muy capacitado! Felicitaciones Mau! Siempre el mejor',
+  rating: 5,
+  author: {
+    name: 'Maritza Medina',
   },
-  openingHours: '',
-  socialLinks: ["www.facebook.com/flux"],
-  testimonials: [
-    {
-      body: '',
-      rating: '',
-      author: {
-        name: '',
-      },
-    },
-  ],
-  features: [
-    {
-      name: 'dfdfdfdfdfd',
-      description: 'dfdfdfdf dfdfd dfd fdf df dfd fd df df df df df dfd ',
-      icon: TrophyIcon,
-    },
-    {
-      name: 'dfdfdfdf df df df ',
-      description: ' df df df dfd df df dfdf df df df df dfdf',
-      icon: TrophyIcon,
-    },
-    {
-      name: 'ewrwerwer ',
-      description: 'werewr erw er wer wer ewrwerewrwerwer ewrwer wer wer wer ewr w',
-      icon: TrophyIcon,
-    },
-  ],
-  images: [
-    {
-      src: '',
-      description: '',
-    },
-  ],
-  faqs: [
-    {
-      question: '',
-      answer: '',
-    },
-  ],
-  placeId: '',
+},{
+  body: 'La mejor atención que he recibido! Aparte de que el lugar es acogedor y cómodo, te reciben siempre de la mejor manera...',
+  rating: 5,
+  author: {
+    name: 'Andrea Matabuena Rivera', 
+  },
+}],
+features: [
+  {
+    name: '🌟 Descargas musculares ',
+    description: 'Revitaliza tus músculos con un masaje profundo, liberándolos de la fatiga y potenciando tu rendimiento',
+    icon: TrophyIcon,
+  },
+  {
+    name: '⚡ Electroterapia',
+    description: 'Usa el poder de la corriente eléctrica para acelerar la recuperación, aliviar el dolor y fortalecer tus músculos',
+    icon: TrophyIcon,
+  },
+  {
+    name: '🏃‍♂️ Ejercicio Terapéutico',
+    description: 'Mejora tu fuerza, movilidad y equilibrio con un enfoque activo y personalizado, elevando tu calidad de vida',
+    icon: TrophyIcon,
+  },
+],
+images:[
+  {
+    src:"https://storage.googleapis.com/web-assets-fluxsales/unityfisiowebsite/WhatsApp%20Image%202023-10-18%20at%205.34.13%20PM%20(2).jpeg",
+    description:"UNITY Fisioterapia en Querétaro"
+  },
+  {
+  src:"https://storage.googleapis.com/web-assets-fluxsales/unityfisiowebsite/WhatsApp%20Image%202023-10-18%20at%205.34.12%20PM%20(1).jpeg",
+  description:"UNITY Fisioterapia en Querétaro"
+},
+{
+  src:"https://storage.googleapis.com/web-assets-fluxsales/unityfisiowebsite/WhatsApp%20Image%202023-10-18%20at%205.34.12%20PM.jpeg",
+  description:"UNITY Fisioterapia en Querétaro"
+},
+{
+  src:"https://storage.googleapis.com/web-assets-fluxsales/unityfisiowebsite/WhatsApp%20Image%202023-10-18%20at%205.34.12%20PM%20(2).jpeg",
+  description:"UNITY Fisioterapia en Querétaro"
+},
+
+{
+  src:"https://storage.googleapis.com/web-assets-fluxsales/unityfisiowebsite/WhatsApp%20Image%202023-10-18%20at%205.34.13%20PM%20(1).jpeg",
+  description:"UNITY Fisioterapia en Querétaro"
+},
+{
+  src:"https://storage.googleapis.com/web-assets-fluxsales/unityfisiowebsite/WhatsApp%20Image%202023-10-18%20at%205.34.13%20PM%20(5).jpeg",
+  description:"UNITY Fisioterapia en Querétaro"
+}
+],
+faqs: [
+  {
+    question: '¿El ejercicio terapéutico es similar al ejercicio regular?',
+    answer: 'Mientras el ejercicio regular está enfocado en mantener o mejorar la condición física general, el ejercicio terapéutico tiene un enfoque más específico. Se dirige a mejorar áreas como la fuerza, movilidad y equilibrio, complementando otros tratamientos de fisioterapia para obtener mejores resultados.',
+  },
+  {
+    question: 'He oído sobre la punción seca, ¿qué es y es dolorosa??',
+    answer: 'La punción seca es una técnica invasiva donde se utiliza una aguja para buscar la relajación y reparación muscular. Si bien puede generar una leve molestia durante su aplicación, muchos pacientes reportan alivio significativo tras el procedimiento.',
+  },
+  {
+    question: '¿Qué diferencia la terapia manual de otros tratamientos?',
+    answer: 'La terapia manual se basa en el uso directo de las manos del fisioterapeuta para tratar molestias y afecciones. Es una técnica que permite un abordaje preciso y personalizado de cada paciente.',
+  }
+],
+placeId:"ChIJzdTjdDBb04UR7bW3Em5cVcc"
 }
 
 /*
