5a7560f1492be030a20366ef27d01de473cdcb0b
Sebastian Septien <sebsepdus@hotmail.com>
2023-10-23 09:23:02 -0600

    Trying to add another way to appear Google Tag Manager

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,20 +1,11 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
-import Script from 'next/script';
 
 const App = ({ Component, pageProps }) =>{
 
   return (
      <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-TMRXP6MB" />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'GTM-TMRXP6MB');
-        `}
-      </Script>
+      <Scrip src="https://www.googletagmanager.com/gtag/js?id=GTM-TMRXP6MB" />
      <Component {...pageProps} />
      </>
   )
