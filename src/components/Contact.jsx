c60b607dbdfedcc32dd24e73ee8a6a4b96d36f88
Sebastian Septien <sebsepdus@hotmail.com>
2023-10-23 12:57:40 -0600

    Adding ids to identify the cta and create a report

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -89,6 +89,7 @@ export default function Contact({ email, phoneNumber }) {
               </div>
               <div>
                 <button
+                  id="cta_contact"
                   type="submit"
                   className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                 >
