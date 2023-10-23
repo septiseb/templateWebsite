6bdf2174963a7531d2d588c3b6d7c8a5a9a06349
Sebastian Septien <sebsepdus@hotmail.com>
2023-10-23 08:46:15 -0600

    Adding the GA in the page to start measuring it

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,22 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import Script from 'next/script';
 
 const App = ({ Component, pageProps }) =>{
 
   return (
-      <Component {...pageProps} />
+     <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-TMRXP6MB" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'GTM-TMRXP6MB');
+        `}
+      </Script>
+     <Component {...pageProps} />
+     </>
   )
 }
 
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,6 +1,4 @@
 import { Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script';
-
 
 const Document = (props) =>{
   let pageProps = props.__NEXT_DATA__?.props?.pageProps
@@ -10,15 +8,6 @@ const Document = (props) =>{
       className="h-full scroll-smooth bg-white antialiased [font-feature-settings:'ss01']"
     >
       <Head>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-TMRXP6MB" />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'GTM-TMRXP6MB');
-        `}
-      </Script>
       </Head>
       <body className="flex h-full flex-col font-display">
         <Main />
