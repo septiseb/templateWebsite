f6c195ea0a5cdf88ac4fcc44505836d43cea175a
Sebastian Septien <sebsepdus@hotmail.com>
2023-10-27 13:35:44 -0600

    Make it larger the letter for better visibility in the mobile version

--- src/components/CTA/OnlineOrder.jsx
+++ src/components/CTA/OnlineOrder.jsx
@@ -7,7 +7,7 @@ export default function OnlineOrder({url}) {
         <button
           id="cta_order"
           type="button"
-          className="relative inline-flex items-center rounded-md bg-gray-600 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+          className="relative inline-flex items-center rounded-md bg-gray-600 px-3 py-2 text-md lg:text-lg  font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
         >
           <PlusIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
           Contáctanos
--- src/services/business.js
+++ src/services/business.js
@@ -1,135 +1,136 @@
 import { TrophyIcon } from '@heroicons/react/20/solid'
 
 export const business = {
-  title: 'Latido',
+  title: 'Lorem Ipsum',
   description:
-    '¡Bienvenido a Latido! Somos tu tienda de accesorios para mujer con precios competitivos. Explora nuestra amplia gama de bisutería y maquillaje, incluyendo esmaltes, aretes, collares y chapa de oro. Trabajamos con marcas reconocidas como Prosa, Bissu y Saniye, garantizando calidad y estilo en cada pieza. Ya sea para tu look diario o una ocasión especial, encuentra el complemento perfecto en Latido y realza tu estilo con nuestras opciones irresistibles. Latido te ofrece la combinación perfecta de moda y asequibilidad. Nuestra colección está cuidadosamente seleccionada para brindarte variedad y calidad en cada elección. Desde esmaltes vibrantes hasta maquillaje de tendencia, nuestros productos resaltarán tu belleza única',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae venenatis leo. Sed fermentum vestibulum justo, non lacinia nunc imperdiet at. Morbi et egestas tortor. Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Nulla facilisi.',
   keywords:
-    'accesorios, mujer, bisutería, maquillaje, esmaltes, aretes, collares, chapa de oro, Saniye, Prosa, Bissu',
-  author: 'Latido - Accesorios para Mujer',
-  email: 'arum_15@gmail.com',
+    'lorem, ipsum, dolor, sit, amet, consectetur, adipiscing, elit, vestibulum, praesent, morbi',
+  author: 'Lorem Ipsum - Dolor Sit Amet',
+  email: 'lorem@ipsum.com',
   image: '',
   url: '',
-  businessName: 'Latido',
+  businessName: 'Lorem Ipsum',
   address: {
-    streetAddress: 'Av Cerro Sombrerete 1199',
-    addressLocality: 'Misión Bucareli Sur',
-    addressRegion: 'Santiago de Querétaro',
-    postalCode: '76168',
-    addressCountry: 'México',
+    streetAddress: '123 Ipsum St.',
+    addressLocality: 'Dolor City',
+    addressRegion: 'Lorem Region',
+    postalCode: '12345',
+    addressCountry: 'Elitland',
   },
-  phone: '5214626059572',
+  phone: '1234567890',
   geo: {
-    latitude: '20.623990',
-    longitude: '-100.412060',
+    latitude: '12.345678',
+    longitude: '98.765432',
   },
   openingHours: "",
-  socialLinks: ["https://www.instagram.com/latidomexico/",""],
+  socialLinks: ["https://www.instagram.com/loremipsum/",""],
   testimonials: [
     {
-      body: 'Excelente atención',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed lacinia urna, eget imperdiet mauris. Morbi a ante ac lacus fringilla tincidunt at eu nunc. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
       rating: 5,
       author: {
-        name: 'Carmen Cruz Servin',
+        name: 'John Doe',
       },
     },
     {
-      body: 'Variedad de cosméticos, me encanta la variedad que hay en los accesorios de mujer',
+      body: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
       rating: 5,
       author: {
-        name: 'Alejandra Ruiz',
+        name: 'Jane Smith',
       },
     },
     {
-      body: 'Buenos precios y buenos accesorios para el regalo de mi esposa!',
+      body: 'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada. Curabitur vel dui eu augue cursus luctus a non risus. Sed nec sapien quis turpis facilisis congue. Etiam ac neque eu lorem volutpat rhoncus. Maecenas tincidunt, orci ut efficitur eleifend, urna ligula molestie ex, ac porttitor diam urna at lacus.',
       rating: 5,
       author: {
-        name: 'Jorge Asdrubal',
+        name: 'James Bond',
       },
     },
     {
-      body: 'Excelentes productos, me encanta todo',
+      body: 'Sed fermentum vestibulum justo, non lacinia nunc imperdiet at. Nullam a est elit. Integer id mauris nec orci efficitur bibendum non a magna. Vivamus in mollis purus. In vulputate, magna eget vehicula finibus, odio metus ultricies dolor, ac sollicitudin elit orci a dui.',
       rating: 5,
       author: {
-        name: 'Alexa Guerrero',
+        name: 'Lucy Heartfilia',
       },
     },
   ],
   features: [
     {
-      name: 'Cosméticos',
+      name: 'Lorem Feature 1',
       description:
-        'Ofrecemos una grande gama de marcas de cosméticos y maquillaje como Saniye, Prossa, Bissu y entre otras marcas.',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et egestas tortor.',
       icon: TrophyIcon,
     },
     {
-      name: 'Bisutería',
+      name: 'Lorem Feature 2',
       description:
-        'Contamos con una amplia gama de aretes, collares, lentes, moños a precios muy accesibles.',
+        'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada.',
       icon: TrophyIcon,
     },
     {
-      name: 'Chapa de Oro',
+      name: 'Lorem Feature 3',
       description:
-        'Tenemos una variedad de aretes y anillos en chapa de oro a precios bastante económicos.',
+        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;',
       icon: TrophyIcon,
     },
   ],
   images: [
     {
-      src: 'https://storage.googleapis.com/web-assets-fluxsales/latidowebsite/WhatsApp%20Image%202023-08-17%20at%2012.00.03%20PM.jpeg',
-      description: 'Latido empresa de ventas de cósmeticos, maquillaje, aretes y muchos accesorios de mujer',
+      src: 'https://www.example.com/image1.jpeg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     },
     {
-      src: 'https://storage.googleapis.com/web-assets-fluxsales/latidowebsite/WhatsApp%20Image%202023-08-17%20at%2012.00.05%20PM%20(1).jpeg',
-      description: 'Latido empresa de ventas de cósmeticos, maquillaje, aretes y muchos accesorios de mujer',
+      src: 'https://www.example.com/image2.jpeg',
+      description: 'Praesent vitae venenatis leo',
     },
     {
-      src: 'https://storage.googleapis.com/web-assets-fluxsales/latidowebsite/WhatsApp%20Image%202023-08-17%20at%2012.00.05%20PM%20(2).jpeg',
-      description: 'Latido empresa de ventas de cósmeticos, maquillaje, aretes y muchos accesorios de mujer',
+      src: 'https://www.example.com/image3.jpeg',
+      description: 'Morbi et egestas tortor',
     },
     {
-      src: 'https://storage.googleapis.com/web-assets-fluxsales/latidowebsite/WhatsApp%20Image%202023-08-17%20at%2012.00.05%20PM.jpeg',
-      description: 'Latido empresa de ventas de cósmeticos, maquillaje, aretes y muchos accesorios de mujer',
+      src: 'https://www.example.com/image4.jpeg',
+      description: 'Sed fermentum vestibulum justo',
     },
     {
-      src: 'https://storage.googleapis.com/web-assets-fluxsales/latidowebsite/WhatsApp%20Image%202023-08-17%20at%2012.00.06%20PM%20(1).jpeg',
-      description: 'Latido empresa de ventas de cósmeticos, maquillaje, aretes y muchos accesorios de mujer',
+      src: 'https://www.example.com/image5.jpeg',
+      description: 'Vivamus euismod purus nec ligula hendrerit',
     },
     {
-      src: 'https://storage.googleapis.com/web-assets-fluxsales/latidowebsite/WhatsApp%20Image%202023-08-17%20at%2012.00.06%20PM.jpeg',
-      description: 'Latido empresa de ventas de cósmeticos, maquillaje, aretes y muchos accesorios de mujer',
+      src: 'https://www.example.com/image6.jpeg',
+      description: 'Vestibulum ante ipsum primis in faucibus',
     }
   ],
   faqs: [
     {
-      question: '¿Manejan marcas originales?',
+      question: 'Lorem ipsum dolor sit amet?',
       answer:
-        'Sí, en Latido manejamos marcas reconocidas y originales como Saniye, Prossa, Bissu, entre otras. Garantizamos calidad y autenticidad en cada producto.',
+        'Consectetur adipiscing elit. Vivamus euismod purus nec ligula hendrerit.',
     },
     {
-      question: '¿Realizan envíos a domicilio?',
+      question: 'Praesent vitae venenatis leo?',
       answer:
-        'Actualmente, ofrecemos un servicio de compra en tienda. Sin embargo, estamos trabajando para implementar envíos a domicilio en el futuro cercano.',
+        'Morbi et egestas tortor. Sed fermentum vestibulum justo.',
     },
     {
-      question: '¿Ofrecen descuentos o promociones?',
+      question: 'Vivamus euismod purus nec ligula?',
       answer:
-        '¡Sí! Con regularidad tenemos promociones y descuentos especiales. Te recomendamos seguirnos en nuestras redes sociales o suscribirte a nuestro boletín para estar al tanto de todas nuestras ofertas.',
+        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;',
     },
     {
-      question: '¿Tienen opciones de maquillaje hipoalergénico?',
+      question: 'Sed fermentum vestibulum justo?',
       answer:
-        'Sí, contamos con una gama de productos hipoalergénicos pensados para pieles sensibles. Puedes preguntar en tienda para que nuestros asesores te guíen hacia las mejores opciones según tus necesidades.',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et egestas tortor.',
     },
     {
-      question: '¿Cuál es el horario de atención de la tienda?',
+      question: 'Morbi et egestas tortor?',
       answer:
-        'Nuestro horario de atención es todos los días de 10:00 am a 8:30 pm. Siempre estamos encantados de atenderte y ayudarte a encontrar el producto perfecto.',
+        'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada.',
     },
   ],
-  placeId: 'ChIJzdTjdDBb04UR7bW3Em5cVcc',
+  placeId: 'LoremIpsumPlaceId12345',
 }
+
 
 /*
 {
