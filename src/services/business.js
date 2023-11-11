17be2a993483e0bdc71ced73130e658eb30f5d34
Sebastian Septien <sebsepdus@hotmail.com>
2023-11-11 13:22:56 -0600

    fixing the json to deploy the website

--- src/services/business.js
+++ src/services/business.js
@@ -2,109 +2,124 @@ import { TrophyIcon } from '@heroicons/react/20/solid'
 
 export const business = {
   title: 'Lenguaje, Audición y Habla',
-    description: 'En Lah, Bienvenido a nuestro espacio dedicado a la Terapia del Lenguaje, donde transformamos las barreras en puentes para una comunicación más fuerte y significativa. Nuestro enfoque único aborda las necesidades de niños, jovenes y adultos con pérdida auditiva que tienen desafíos en la oralización y la lectoescritura.',
-    keywords: 'Terapia de lenguaje y de habla, oralización para niños pérdidas auditivas, jóvenes y adultos con problemas auditivos',
-    author: 'Lic. Maria del Carmen Campos',
-    email: 'lah_mx@hotmail.com',
-    image: 'Terapia de Lenguaje en Celaya',
-    url: 'terapiacaramelo.netlify.app',
-    businessName: 'Lenguaje, Audición y Habla',
-    address: {
-      streetAddress: ' Coahuila 903, Alameda',
-      addressLocality: 'Celaya',
-      addressRegion: 'Gto',
-      postalCode: '38050',
-      addressCountry: 'Mexico',
-    },
-    phone: '5530109993',
-    geo: {
-      latitude: '20.530817181258914', 
-      longitude: '-100.80800428968116',
-    },
-    openingHours: 'Mo,Tu,We,Th,Fr 09:00-14:00, 16:00-19:45; Sa,Su Closed',
-    socialLinks: [ 
-      'https://www.facebook.com/lenguajeaudicionyhabla/',
-      'https://www.instagram.com/lah_mx/',
-      ],
-    testimonials: [{
+  description:
+    'En Lah, Bienvenido a nuestro espacio dedicado a la Terapia del Lenguaje, donde transformamos las barreras en puentes para una comunicación más fuerte y significativa. Nuestro enfoque único aborda las necesidades de niños, jovenes y adultos con pérdida auditiva que tienen desafíos en la oralización y la lectoescritura.',
+  keywords:
+    'Terapia de lenguaje y de habla, oralización para niños pérdidas auditivas, jóvenes y adultos con problemas auditivos',
+  author: 'Lic. Maria del Carmen Campos',
+  email: 'lah_mx@hotmail.com',
+  image: 'Terapia de Lenguaje en Celaya',
+  url: 'terapiacaramelo.netlify.app',
+  businessName: 'Lenguaje, Audición y Habla',
+  address: {
+    streetAddress: ' Coahuila 903, Alameda',
+    addressLocality: 'Celaya',
+    addressRegion: 'Gto',
+    postalCode: '38050',
+    addressCountry: 'Mexico',
+  },
+  phone: '5530109993',
+  geo: {
+    latitude: '20.530817181258914',
+    longitude: '-100.80800428968116',
+  },
+  openingHours: 'Mo,Tu,We,Th,Fr 09:00-14:00, 16:00-19:45; Sa,Su Closed',
+  socialLinks: [
+    'https://www.facebook.com/lenguajeaudicionyhabla/',
+    'https://www.instagram.com/lah_mx/',
+  ],
+  testimonials: [
+    {
       body: 'Super profesional. Muy honesta y con una increíble paciencia no solo con mi hijo sino también conmigo como mamá ',
       rating: 5,
       author: {
         name: 'Adriana López',
       },
-       },{
+    },
+    {
       body: 'La mejor terapeuta excelente trabajo muy profesional esta al pendiente  de todos los estudios, audiometrias comportamiento del niño, uno ve avances muy rápido en el habla del niño , muy comprometida en su trabajo',
       rating: 5,
       author: {
-        name: 'Erendira González'},
-    },{
+        name: 'Erendira González',
+      },
+    },
+    {
       body: 'Estoy muy contenta de que me recomendaran a la licenciada Carmen, ya que mi hijo tuvo muchos problemas con el lenguaje y no hablaba nada; y desde que acude a terapia con ella, el niño a través de ejercicios diversos, logró hablar y cada vez su pronunciación mejora bastante.',
       rating: 5,
       author: {
         name: 'Abril Rubio',
       },
-    },{
+    },
+    {
       body: 'Excelente terapeuta de lenguaje y atención de personas con déficit auditivo. El lugar también es maravilloso.',
       rating: 5,
       author: {
-        name: 'Beatriz Gomez', 
+        name: 'Beatriz Gomez',
       },
-    }],
-    features: [
-      {
-        name: '🗣️ Terapia de Lenguaje',
-        description: 'Mejora tus habilidades de comprensión y expresión del lenguaje. Desde gramática hasta vocabulario y fluidez, desata todo tu potencial lingüístico.',
-        icon: TrophyIcon,
-      },
-      {
-        name: '🎤 Terapia de Habla (Pronunciación)',
-        description: 'Perfecciona la pronunciación para una comunicación clara y efectiva. Transforma tu expresión oral y conquista la confianza en cada palabra.',
-        icon: TrophyIcon,
-      },
-      {
-        name: '👂 Oralización de Niños con Pérdidas Auditivas',
-        description: 'Especialistas en implantes cocleares y aparatos auditivos para potenciar habilidades de comunicación oral y auditiva en niños. Abre las puertas a un mundo sonoro pleno.',
-        icon: TrophyIcon,
-      },
-    ],
-    images:[{
-      src:"https://storage.googleapis.com/web-assets-fluxsales/carameloterapia/WhatsApp%20Image%202023-10-30%20at%206.50.30%20PM%20(1).jpeg",
-      description:"Terapia de Lenguaje en Celaya"
+    },
+  ],
+  features: [
+    {
+      name: '🗣️ Terapia de Lenguaje',
+      description:
+        'Mejora tus habilidades de comprensión y expresión del lenguaje. Desde gramática hasta vocabulario y fluidez, desata todo tu potencial lingüístico.',
+      icon: TrophyIcon,
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/carameloterapia/WhatsApp%20Image%202023-10-30%20at%206.50.30%20PM.jpeg",
-      description:"Terapia de Lenguaje en Celaya"
+      name: '🎤 Terapia de Habla (Pronunciación)',
+      description:
+        'Perfecciona la pronunciación para una comunicación clara y efectiva. Transforma tu expresión oral y conquista la confianza en cada palabra.',
+      icon: TrophyIcon,
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/carameloterapia/WhatsApp%20Image%202023-11-08%20at%2010.13.38%20AM.jpeg",
-      description:"Terapia de Lenguaje en Celaya"
+      name: '👂 Oralización de Niños con Pérdidas Auditivas',
+      description:
+        'Especialistas en implantes cocleares y aparatos auditivos para potenciar habilidades de comunicación oral y auditiva en niños. Abre las puertas a un mundo sonoro pleno.',
+      icon: TrophyIcon,
+    },
+  ],
+  images: [
+    {
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/carameloterapia/WhatsApp%20Image%202023-11-08%20at%2010.13.38%20AM.jpeg',
+      description: 'Terapia de Lenguaje en Celaya',
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/carameloterapia/WhatsApp%20Image%202023-11-08%20at%2010.13.39%20AM%20(1).jpeg",
-      description:"Lenguaje, Audición y Habla"
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/carameloterapia/WhatsApp%20Image%202023-11-08%20at%2010.13.39%20AM.jpeg',
+      description: 'Terapia de Lenguaje en Celaya',
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/carameloterapia/WhatsApp%20Image%202023-11-08%20at%2010.13.39%20AM.jpeg",
-      description:"Terapia de Lenguaje en Celaya"
-    }
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/carameloterapia/WhatsApp%20Image%202023-11-08%20at%2010.13.39%20AM%20(1).jpeg',
+      description: 'Lenguaje, Audición y Habla',
+    },
+    {
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/carameloterapia/WhatsApp%20Image%202023-10-30%20at%206.50.30%20PM%20(1).jpeg',
+      description: 'Terapia de Lenguaje en Celaya',
+    },
+    {
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/carameloterapia/WhatsApp%20Image%202023-10-30%20at%206.50.30%20PM.jpeg',
+      description: 'Terapia de Lenguaje en Celaya',
+    },
   ],
   faqs: [
-      {
-        question: '¿En qué consiste la Terapia de Lenguaje?',
-        answer: 'La Terapia de Lenguaje es un programa especializado que mejora habilidades de comprensión y expresión del lenguaje, abordando gramática, vocabulario y fluidez.',
-      },
-      {
-        question: '¿Cómo beneficia la Terapia de Habla (Pronunciación)?',
-        answer: 'La Terapia de Habla perfecciona la pronunciación para lograr una comunicación más clara y efectiva, potenciando la confianza en la expresión oral.',
-      },
-      {
-        question: '¿Cómo ayuda la Rehabilitación a Jóvenes y Adultos con pérdida auditiva?',
-        answer: 'Ofrecemos apoyo comprensivo en la adaptación a la pérdida auditiva, con estrategias efectivas para recuperar la conexión con el mundo sonoro.',
-      }
-    ],
-    placeId:"ChIJGaKwOxW7LIQRJ0RZP2xk4tc"
+    {
+      question: '¿En qué consiste la Terapia de Lenguaje?',
+      answer:
+        'La Terapia de Lenguaje es un programa especializado que mejora habilidades de comprensión y expresión del lenguaje, abordando gramática, vocabulario y fluidez.',
+    },
+    {
+      question: '¿Cómo beneficia la Terapia de Habla (Pronunciación)?',
+      answer:
+        'La Terapia de Habla perfecciona la pronunciación para lograr una comunicación más clara y efectiva, potenciando la confianza en la expresión oral.',
+    },
+    {
+      question:
+        '¿Cómo ayuda la Rehabilitación a Jóvenes y Adultos con pérdida auditiva?',
+      answer:
+        'Ofrecemos apoyo comprensivo en la adaptación a la pérdida auditiva, con estrategias efectivas para recuperar la conexión con el mundo sonoro.',
+    },
+  ],
+  placeId: 'ChIJGaKwOxW7LIQRJ0RZP2xk4tc',
 }
-
 
 /*
 {
