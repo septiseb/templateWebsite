a0268eb4e27515cb2b26e47f3c724aed6b93eb18
Sebastian Septien <sebsepdus@hotmail.com>
2023-11-27 13:57:31 -0600

    Adding the photos and erasing an extra review

--- src/services/business.js
+++ src/services/business.js
@@ -1,114 +1,109 @@
 import { TrophyIcon } from '@heroicons/react/20/solid'
 
-export const business = {title: 'Dra. Yazmin Morales Implantología y Estética Dental',
-description: '¡Descubre la Sonrisa Perfecta con nosotros! Ofrecemos diagnósticos y tratamientos preventivos, limpieza dental, diseño de sonrisa digital, impresiones digitales y tratamientos avanzados como carillas, implantes, restauraciones libres de metal. Transforma tu sonrisa hoy.',
-keywords: 'Diagnóstico y tratamientos preventivos, Limpieza Dental, Diseño de sonrisa digital, carillas, implantes, restauraciones libres de metal',
-author: 'Dra. Yazmin Morales Implantología y Estética Dental',
-email: 'drayazmin.ms@hotmail.com',
-image: '',
-url: '',
-businessName: 'Dra. Yazmin Morales Implantología y Estética Dental',
-address: {
-  streetAddress: 'a Celaya, Plaza 4 Quince Local 5, C. San Miguel Km 3',
-  addressLocality: 'San Miguel de Allende',
-  addressRegion: 'Gto',
-  postalCode: '37700',
-  addressCountry: 'Mexico',
-},
-phone: ' 4151157121',
-geo: {
-  latitude: ' 20.923211545520328',
-  longitude: '-100.757809097904',
-},
-openingHours: 'Mo-Fr 09:00-14:00, Mo-Fr 17:00-19:00, Sa 09:00-14:00',
-socialLinks: [ 
-  'https://www.facebook.com/drayazminmorales/',
-  '',
+export const business = {
+  title: 'Dra. Yazmin Morales Implantología y Estética Dental',
+  description:
+    '¡Descubre la Sonrisa Perfecta con nosotros! Ofrecemos diagnósticos y tratamientos preventivos, limpieza dental, diseño de sonrisa digital, impresiones digitales y tratamientos avanzados como carillas, implantes, restauraciones libres de metal. Transforma tu sonrisa hoy.',
+  keywords:
+    'Diagnóstico y tratamientos preventivos, Limpieza Dental, Diseño de sonrisa digital, carillas, implantes, restauraciones libres de metal',
+  author: 'Dra. Yazmin Morales Implantología y Estética Dental',
+  email: 'drayazmin.ms@hotmail.com',
+  image: '',
+  url: '',
+  businessName: 'Dra. Yazmin Morales Implantología y Estética Dental',
+  address: {
+    streetAddress: 'a Celaya, Plaza 4 Quince Local 5, C. San Miguel Km 3',
+    addressLocality: 'San Miguel de Allende',
+    addressRegion: 'Gto',
+    postalCode: '37700',
+    addressCountry: 'Mexico',
+  },
+  phone: ' 4151157121',
+  geo: {
+    latitude: ' 20.923211545520328',
+    longitude: '-100.757809097904',
+  },
+  openingHours: 'Mo-Fr 09:00-14:00, Mo-Fr 17:00-19:00, Sa 09:00-14:00',
+  socialLinks: ['https://www.facebook.com/drayazminmorales/', ''],
+  testimonials: [
+    {
+      body: 'Me encantó la atención de la Dra. Yazmin y lo mejor de todo es como mejoró mi sonrisa.',
+      rating: 5,
+      author: {
+        name: 'Sebastian',
+      },
+    },
+    {
+      body: 'Increíble atención por parte de la Dra. Yazmin. Mi sonrisa nunca ha lucido mejor. ¡Muy agradecido!',
+      rating: 5,
+      author: {
+        name: 'Jorge M',
+      },
+    },
+    {
+      body: 'La Dra. Yazmin es una profesional excepcional. Su atención hizo que mi experiencia dental fuera maravillosa. ¡Mi sonrisa ahora es mi mejor accesorio!',
+      rating: 5,
+      author: {
+        name: 'Valentina',
+      },
+    },
   ],
-testimonials: [{
-  body: 'Me encantó la atención de la Dra. Yazmin y lo mejor de todo es como mejoró mi sonrisa.',
-  rating: 5,
-  author: {
-    name: 'Sebastian',
-  },
-   },{
-  body: 'Increíble atención por parte de la Dra. Yazmin. Mi sonrisa nunca ha lucido mejor. ¡Muy agradecido!',
-  rating: 5,
-  author: {
-    name: 'Jorge M'
-  },
-},{
-  body: 'La Dra. Yazmin es una profesional excepcional. Su atención hizo que mi experiencia dental fuera maravillosa. ¡Mi sonrisa ahora es mi mejor accesorio!',
-  rating: 5,
-  author: {
-    name: 'Valentina',
-  },
-},{
-  body: '',
-  rating: 5,
-  author: {
-    name: '', 
-  },
-}],
-features: [
-  {
-    name: 'Diseño digital de sonrisa',
-    description: 'Mejora tu sonrisa con nuestra previsualización digital. Antes de realizar cambios en tu boca, visualiza tu tratamiento con confianza.',
-    icon: TrophyIcon,
-  },
-  {
-    name: 'Carillas',
-    description: 'Transforma el color y la forma de tus dientes con un diagnóstico preciso y un plan de tratamiento. Logra resultados sorprendentes con un mínimo desgaste dental.',
-    icon: TrophyIcon,
-  },
-  {
-    name: 'Implantes dentales',
-    description: 'Recupera una zona donde has perdido uno o varios dientes de manera fija, sin afectar los dientes adyacentes ni recurrir a prótesis removibles. Descubre una solución de rehabilitación dental sin complicaciones.',
-    icon: TrophyIcon,
-  },
-  
-],
-images:[{
-  src:"",
-  description:"Dra. Yazmin Morales Implantología y Estética Dental"
-},
-{
-  src:"",
-  description:"Dra. Yazmin Morales Implantología y Estética Dental"
-},
-{
-  src:"",
-  description:"Dra. Yazmin Morales Implantología y Estética Dental"
-},
-{
-  src:"",
-  description:"Dra. Yazmin Morales Implantología y Estética Dental"
-},
-{
-  src:"",
-  description:"Dra. Yazmin Morales Implantología y Estética Dental"
-},
-{
-  src:"",
-  description:"Dra. Yazmin Morales Implantología y Estética Dental"
+  features: [
+    {
+      name: 'Diseño digital de sonrisa',
+      description:
+        'Mejora tu sonrisa con nuestra previsualización digital. Antes de realizar cambios en tu boca, visualiza tu tratamiento con confianza.',
+      icon: TrophyIcon,
+    },
+    {
+      name: 'Carillas',
+      description:
+        'Transforma el color y la forma de tus dientes con un diagnóstico preciso y un plan de tratamiento. Logra resultados sorprendentes con un mínimo desgaste dental.',
+      icon: TrophyIcon,
+    },
+    {
+      name: 'Implantes dentales',
+      description:
+        'Recupera una zona donde has perdido uno o varios dientes de manera fija, sin afectar los dientes adyacentes ni recurrir a prótesis removibles. Descubre una solución de rehabilitación dental sin complicaciones.',
+      icon: TrophyIcon,
+    },
+  ],
+  images: [
+    {
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/yazmindentista/WhatsApp%20Image%202023-11-17%20at%203.27.36%20PM.jpeg',
+      description: 'Dra. Yazmin Morales Implantología y Estética Dental',
+    },
+    {
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/yazmindentista/WhatsApp%20Image%202023-11-17%20at%203.45.40%20PM.jpeg',
+      description: 'Dra. Yazmin Morales Implantología y Estética Dental',
+    },
+    {
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/yazmindentista/WhatsApp%20Image%202023-11-17%20at%206.33.08%20PM.jpeg',
+      description: 'Dra. Yazmin Morales Implantología y Estética Dental',
+    },
+  ],
+  faqs: [
+    {
+      question:
+        '¿Cuáles son los beneficios de la previsualización digital en tratamientos dentales?',
+      answer:
+        'La previsualización digital permite a los pacientes visualizar cómo se verá su sonrisa después del tratamiento, brindando confianza y claridad antes de realizar cambios en la boca. Es una herramienta valiosa para personalizar y planificar procedimientos dentales.',
+    },
+    {
+      question:
+        '¿Cómo funciona la transformación dental con un mínimo desgaste dental?',
+      answer:
+        'La transformación dental con mínimo desgaste implica utilizar técnicas y tecnologías avanzadas que preservan la estructura dental natural. Esto permite cambios estéticos y funcionales sin comprometer la salud de los dientes adyacentes.',
+    },
+    {
+      question:
+        '¿Cómo se diferencia el diseño de sonrisa digital en su enfoque de mejora estética?',
+      answer:
+        'El diseño de sonrisa digital es una herramienta avanzada que utilizamos para planificar y personalizar tu tratamiento estético. Permite una visualización precisa de los resultados deseados, brindándote una experiencia única y participativa en la transformación de tu sonrisa.',
+    },
+  ],
+  placeId: 'ChIJ_aZxFZpRK4QRKXB-zkOazf8',
 }
-],
-faqs: [
-  {
-    question: '¿Cuáles son los beneficios de la previsualización digital en tratamientos dentales?',
-    answer: 'La previsualización digital permite a los pacientes visualizar cómo se verá su sonrisa después del tratamiento, brindando confianza y claridad antes de realizar cambios en la boca. Es una herramienta valiosa para personalizar y planificar procedimientos dentales.',
-  },
-  {
-    question: '¿Cómo funciona la transformación dental con un mínimo desgaste dental?',
-    answer: 'La transformación dental con mínimo desgaste implica utilizar técnicas y tecnologías avanzadas que preservan la estructura dental natural. Esto permite cambios estéticos y funcionales sin comprometer la salud de los dientes adyacentes.',
-  },
-  {
-    question: '¿Cómo se diferencia el diseño de sonrisa digital en su enfoque de mejora estética?',
-    answer: 'El diseño de sonrisa digital es una herramienta avanzada que utilizamos para planificar y personalizar tu tratamiento estético. Permite una visualización precisa de los resultados deseados, brindándote una experiencia única y participativa en la transformación de tu sonrisa.',
-  }
-],
-placeId:"ChIJ_aZxFZpRK4QRKXB-zkOazf8"}
-
 
 /*
 {
