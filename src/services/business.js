6050dacc3c12c2d0893fa48f9d8f2f964a7a5a10
Sebastian Septien <sebsepdus@hotmail.com>
2023-11-27 14:37:37 -0600

    Adding the the page of dentista montalvan

--- src/services/business.js
+++ src/services/business.js
@@ -1,136 +1,107 @@
 import { TrophyIcon } from '@heroicons/react/20/solid'
 
 export const business = {
-  title: 'Lorem Ipsum',
+  title: 'Dr. Daniel Montalvan Aguilar',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae venenatis leo. Sed fermentum vestibulum justo, non lacinia nunc imperdiet at. Morbi et egestas tortor. Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Nulla facilisi.',
+    '¡Bienvenido a la excelencia en odontología! Soy el Dr. Daniel Montalvan, Cirujano Dentista especializado en Implantología. Con años de experiencia, ofrezco soluciones avanzadas para tu salud bucal. Descubre un servicio personalizado y resultados duraderos, desde implantes dentales hasta procedimientos de vanguardia. ¡Recupera la funcionalidad y estética de tu sonrisa hoy mismo!',
   keywords:
-    'lorem, ipsum, dolor, sit, amet, consectetur, adipiscing, elit, vestibulum, praesent, morbi',
-  author: 'Lorem Ipsum - Dolor Sit Amet',
-  email: 'lorem@ipsum.com',
+    'Implantes dentales, cirugías de regeneración ósea, cirugía de encías, cirugía plástica periodontal, rehabilitación bucal, prótesis sobre implantes',
+  author: 'Dr. Daniel Montalvan Aguilar',
+  email: 'dmimplantologia@gmail.com',
   image: '',
   url: '',
-  businessName: 'Lorem Ipsum',
+  businessName: 'Dr. Daniel Montalvan Aguilar',
   address: {
-    streetAddress: '123 Ipsum St.',
-    addressLocality: 'Dolor City',
-    addressRegion: 'Lorem Region',
-    postalCode: '12345',
-    addressCountry: 'Elitland',
+    streetAddress:
+      'Hospital Moscati, consul 2314, Av. de las Ciencias 2058, Juriquilla',
+    addressLocality: 'Querétaro',
+    addressRegion: 'Qro',
+    postalCode: '76320',
+    addressCountry: 'Mexico',
   },
-  phone: '1234567890',
+  phone: '4427767348',
   geo: {
-    latitude: '12.345678',
-    longitude: '98.765432',
+    latitude: ' 20.69188867948479',
+    longitude: '-100.43773590415923',
   },
-  openingHours: "",
-  socialLinks: ["https://www.instagram.com/loremipsum/",""],
+  openingHours: 'Mo-Sa 10:30-19:00, Su Closed',
+  socialLinks: ['https://www.facebook.com/dmimplantologia/?locale=es_LA', ''],
   testimonials: [
     {
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed lacinia urna, eget imperdiet mauris. Morbi a ante ac lacus fringilla tincidunt at eu nunc. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+      body: 'Excelente y amable atención y muy clara la explicación sobre los trabajos requeridos.',
       rating: 5,
       author: {
-        name: 'John Doe',
+        name: 'Luis Valiente',
       },
     },
     {
-      body: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      body: 'Recomiendo al dr. Dany por su profesionalismo y calidad, es empático, atento y toma en cuenta las necesidades de su paciente.',
       rating: 5,
       author: {
-        name: 'Jane Smith',
+        name: 'Open Skull',
       },
     },
     {
-      body: 'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada. Curabitur vel dui eu augue cursus luctus a non risus. Sed nec sapien quis turpis facilisis congue. Etiam ac neque eu lorem volutpat rhoncus. Maecenas tincidunt, orci ut efficitur eleifend, urna ligula molestie ex, ac porttitor diam urna at lacus.',
+      body: 'Dr con años de experiencia, ademas de que todo explica con super detalle. Super recomendado!!',
       rating: 5,
       author: {
-        name: 'James Bond',
-      },
-    },
-    {
-      body: 'Sed fermentum vestibulum justo, non lacinia nunc imperdiet at. Nullam a est elit. Integer id mauris nec orci efficitur bibendum non a magna. Vivamus in mollis purus. In vulputate, magna eget vehicula finibus, odio metus ultricies dolor, ac sollicitudin elit orci a dui.',
-      rating: 5,
-      author: {
-        name: 'Lucy Heartfilia',
+        name: 'George',
       },
     },
   ],
   features: [
     {
-      name: 'Lorem Feature 1',
+      name: 'Implantes Dentales',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et egestas tortor.',
+        'Recupera tu sonrisa con nuestros implantes dentales. Restauramos la funcionalidad y estética dental de forma personalizada. ¡Confía en nosotros para una sonrisa duradera y natural!',
       icon: TrophyIcon,
     },
     {
-      name: 'Lorem Feature 2',
+      name: 'Cirugías de Regeneración Ósea',
       description:
-        'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada.',
+        'Revitaliza tu sonrisa con cirugías de regeneración ósea. Restauramos tu estructura dental con precisión para una salud bucal óptima.',
       icon: TrophyIcon,
     },
     {
-      name: 'Lorem Feature 3',
+      name: 'Cirugía Plástica Periodontal',
       description:
-        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;',
+        'Transforma tu sonrisa con nuestra cirugía plástica periodontal. Expertos en mejorar la estética de las encías, ofrecemos soluciones personalizadas para realzar la belleza natural de tu sonrisa. Descubre cómo podemos revitalizar tu salud bucal y brindarte una apariencia radiante con nuestras técnicas avanzadas.',
       icon: TrophyIcon,
     },
   ],
   images: [
     {
-      src: 'https://www.example.com/image1.jpeg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/dentistamontalvan/WhatsApp%20Image%202023-11-15%20at%2012.02.20%20PM.jpeg',
+      description: 'Dr. Daniel Montalvan Aguilar',
     },
     {
-      src: 'https://www.example.com/image2.jpeg',
-      description: 'Praesent vitae venenatis leo',
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/dentistamontalvan/WhatsApp%20Image%202023-11-20%20at%2010.29.42%20PM.jpeg',
+      description: 'Dr. Daniel Montalvan Aguilar',
     },
     {
-      src: 'https://www.example.com/image3.jpeg',
-      description: 'Morbi et egestas tortor',
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/dentistamontalvan/WhatsApp%20Image%202023-11-15%20at%2012.02.22%20PM.jpeg',
+      description: 'Dr. Daniel Montalvan Aguilar',
     },
-    {
-      src: 'https://www.example.com/image4.jpeg',
-      description: 'Sed fermentum vestibulum justo',
-    },
-    {
-      src: 'https://www.example.com/image5.jpeg',
-      description: 'Vivamus euismod purus nec ligula hendrerit',
-    },
-    {
-      src: 'https://www.example.com/image6.jpeg',
-      description: 'Vestibulum ante ipsum primis in faucibus',
-    }
   ],
   faqs: [
     {
-      question: 'Lorem ipsum dolor sit amet?',
+      question: '¿Cómo ayuda la regeneración ósea en la odontología?',
       answer:
-        'Consectetur adipiscing elit. Vivamus euismod purus nec ligula hendrerit.',
+        'La regeneración ósea es clave para fortalecer la estructura dental. Se utiliza para mejorar la calidad y cantidad del hueso, proporcionando una base sólida para implantes u otros procedimientos.',
     },
     {
-      question: 'Praesent vitae venenatis leo?',
+      question: '¿En qué casos se recomienda la cirugía de encías?',
       answer:
-        'Morbi et egestas tortor. Sed fermentum vestibulum justo.',
+        'La cirugía de encías aborda problemas como la recesión gingival. La cirugía plástica periodontal se centra en mejorar la estética de las encías para una sonrisa más armoniosa.',
     },
     {
-      question: 'Vivamus euismod purus nec ligula?',
+      question: '¿Qué implica exactamente la rehabilitación bucal?',
       answer:
-        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;',
-    },
-    {
-      question: 'Sed fermentum vestibulum justo?',
-      answer:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et egestas tortor.',
-    },
-    {
-      question: 'Morbi et egestas tortor?',
-      answer:
-        'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada.',
+        'La rehabilitación bucal se refiere a la restauración integral de la salud oral, abordando problemas funcionales y estéticos mediante diversos tratamientos.',
     },
   ],
-  placeId: 'LoremIpsumPlaceId12345',
+  placeId: 'ChIJI2GMKp5Z04URj80Zk04HgPo',
 }
-
 
 /*
 {
