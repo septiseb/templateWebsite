8778c1df196b8f17e3d77fa1ebaa3dcdb8cf5529
Sebastian Septien <sebsepdus@hotmail.com>
2023-11-29 19:12:05 -0600

    ltripMexico page

--- src/services/business.js
+++ src/services/business.js
@@ -1,135 +1,114 @@
 import { TrophyIcon } from '@heroicons/react/20/solid'
 
 export const business = {
-  title: 'Lorem Ipsum',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae venenatis leo. Sed fermentum vestibulum justo, non lacinia nunc imperdiet at. Morbi et egestas tortor. Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Nulla facilisi.',
-  keywords:
-    'lorem, ipsum, dolor, sit, amet, consectetur, adipiscing, elit, vestibulum, praesent, morbi',
-  author: 'Lorem Ipsum - Dolor Sit Amet',
-  email: 'lorem@ipsum.com',
-  image: '',
-  url: '',
-  businessName: 'Lorem Ipsum',
-  address: {
-    streetAddress: '123 Ipsum St.',
-    addressLocality: 'Dolor City',
-    addressRegion: 'Lorem Region',
-    postalCode: '12345',
-    addressCountry: 'Elitland',
+  title: 'LTrip',
+description: 'LTrip convierte tus sueños de viaje en experiencias extraordinarias. Descubre aventuras únicas diseñadas para maximizar tu felicidad. De destinos icónicos a lugares exóticos, hacemos que cada viaje sea inolvidable. Únete a nosotros para vivir la mejor versión de tus sueños de viaje con LTrip.',
+keywords: 'Festivales de Música, Eventos Deportivos Mundiales, Viajes Personalizados',
+author: 'LTrip',
+email: 'contact@ltrip.mx',
+image: '',
+url: 'ltripmexico.netlify.app',
+businessName: 'LTrip',
+address: {
+  streetAddress: 'Mexico',
+  addressLocality: 'Mexico',
+  addressRegion: 'Mexico',
+  postalCode: '',
+  addressCountry: 'Mexico',
+},
+phone: '4424671883',
+geo: {
+  latitude: '',  
+  longitude: '',
+},
+openingHours: 'Always Open',
+socialLinks: [ 
+  'https://www.instagram.com/ltrip_/',
+  '',
+  ],
+testimonials: [{
+  body: 'Ltrip hizo de nuestro viaje al festival de música una experiencia única. Desde la organización impecable hasta los detalles sorprendentes, superaron nuestras expectativas. ¡Gracias por convertir un simple viaje en recuerdos inolvidables!',
+  rating: 5,
+  author: {
+    name: 'Ana Martínez',
   },
-  phone: '1234567890',
-  geo: {
-    latitude: '12.345678',
-    longitude: '98.765432',
+   },{
+  body: 'Increíble experiencia en el evento deportivo que siempre soñé asistir. Ltrip se encargó de todos los detalles, permitiéndome disfrutar al máximo cada momento. Definitivamente, la mejor agencia para vivir eventos deportivos a nivel mundial.',
+  rating: 5,
+  author: {
+    name: 'Javier'
   },
-  openingHours: "",
-  socialLinks: ["https://www.instagram.com/loremipsum/",""],
-  testimonials: [
-    {
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed lacinia urna, eget imperdiet mauris. Morbi a ante ac lacus fringilla tincidunt at eu nunc. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-      rating: 5,
-      author: {
-        name: 'John Doe',
-      },
-    },
-    {
-      body: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-      rating: 5,
-      author: {
-        name: 'Jane Smith',
-      },
-    },
-    {
-      body: 'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada. Curabitur vel dui eu augue cursus luctus a non risus. Sed nec sapien quis turpis facilisis congue. Etiam ac neque eu lorem volutpat rhoncus. Maecenas tincidunt, orci ut efficitur eleifend, urna ligula molestie ex, ac porttitor diam urna at lacus.',
-      rating: 5,
-      author: {
-        name: 'James Bond',
-      },
-    },
-    {
-      body: 'Sed fermentum vestibulum justo, non lacinia nunc imperdiet at. Nullam a est elit. Integer id mauris nec orci efficitur bibendum non a magna. Vivamus in mollis purus. In vulputate, magna eget vehicula finibus, odio metus ultricies dolor, ac sollicitudin elit orci a dui.',
-      rating: 5,
-      author: {
-        name: 'Lucy Heartfilia',
-      },
-    },
-  ],
-  features: [
-    {
-      name: 'Lorem Feature 1',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et egestas tortor.',
-      icon: TrophyIcon,
-    },
-    {
-      name: 'Lorem Feature 2',
-      description:
-        'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada.',
-      icon: TrophyIcon,
-    },
-    {
-      name: 'Lorem Feature 3',
-      description:
-        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;',
-      icon: TrophyIcon,
-    },
-  ],
-  images: [
-    {
-      src: 'https://www.example.com/image1.jpeg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    },
-    {
-      src: 'https://www.example.com/image2.jpeg',
-      description: 'Praesent vitae venenatis leo',
-    },
-    {
-      src: 'https://www.example.com/image3.jpeg',
-      description: 'Morbi et egestas tortor',
-    },
-    {
-      src: 'https://www.example.com/image4.jpeg',
-      description: 'Sed fermentum vestibulum justo',
-    },
-    {
-      src: 'https://www.example.com/image5.jpeg',
-      description: 'Vivamus euismod purus nec ligula hendrerit',
-    },
-    {
-      src: 'https://www.example.com/image6.jpeg',
-      description: 'Vestibulum ante ipsum primis in faucibus',
-    }
-  ],
-  faqs: [
-    {
-      question: 'Lorem ipsum dolor sit amet?',
-      answer:
-        'Consectetur adipiscing elit. Vivamus euismod purus nec ligula hendrerit.',
-    },
-    {
-      question: 'Praesent vitae venenatis leo?',
-      answer:
-        'Morbi et egestas tortor. Sed fermentum vestibulum justo.',
-    },
-    {
-      question: 'Vivamus euismod purus nec ligula?',
-      answer:
-        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;',
-    },
-    {
-      question: 'Sed fermentum vestibulum justo?',
-      answer:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et egestas tortor.',
-    },
-    {
-      question: 'Morbi et egestas tortor?',
-      answer:
-        'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada.',
-    },
-  ],
-  placeId: 'LoremIpsumPlaceId12345',
+},{
+  body: 'Nuestro viaje personalizado con Ltrip fue más allá de nuestras expectativas. Cada detalle fue cuidadosamente planificado, haciendo que nuestro viaje de graduación sea verdaderamente especial. Recomendamos totalmente sus servicios para experiencias personalizadas e inolvidables.',
+  rating: 5,
+  author: {
+    name: 'George',
+  },
+},{
+  body: 'Ltrip no solo organiza viajes, sino que crea momentos que duran toda la vida. Su enfoque en la experiencia humana se refleja en cada detalle, convirtiendo cada ocasión en un recuerdo precioso. ¡Gracias por hacer que nuestra despedida de soltero sea única!',
+  rating: 5,
+  author: {
+    name: 'Charly', 
+  },
+}],
+features: [
+  {
+    name: 'Paquetes Exclusivos para Festivales de Música',
+    description: 'Sumérgete en la vibrante energía de los festivales de música con nuestros paquetes exclusivos. Desde artistas de renombre hasta ubicaciones impresionantes, cada detalle está cuidadosamente curado para que experimentes la música de una manera que nunca olvidarás.',
+    icon: TrophyIcon,
+  },
+  {
+    name: 'Experiencias Inolvidables en Eventos Deportivos Mundiales',
+    description: 'Te llevamos a la primera fila de los eventos deportivos más destacados del mundo. Desde emocionantes partidos hasta competiciones legendarias, en Ltrip te garantizamos una experiencia que se quedará grabada en tu memoria para siempre.',
+    icon: TrophyIcon,
+  },
+  {
+    name: 'Viajes Personalizados para Momentos Especiales',
+    description: 'Desde viajes de graduación hasta despedidas de solteros, creamos itinerarios personalizados que superan todas las expectativas. Tu ocasión especial merece un viaje a medida, y en Ltrip nos aseguramos de que cada detalle sea perfecto.',
+    icon: TrophyIcon,
+  },
+  
+],
+images:[{
+  src:"",
+  description:"LTrip"
+},
+{
+  src:"",
+  description:"LTrip"
+},
+{
+  src:"",
+  description:"LTrip"
+},
+{
+  src:"",
+  description:"LTrip"
+},
+{
+  src:"",
+  description:"LTrip"
+},
+{
+  src:"",
+  description:"LTrip"
 }
+],
+faqs: [
+  {
+    question: '¿Qué hace a Ltrip diferente de otras agencias de viajes?',
+    answer: 'En Ltrip, nos destacamos por nuestra atención personalizada y la creación de experiencias únicas. Desde festivales de música hasta eventos deportivos, nos esforzamos por superar las expectativas y convertir cada viaje en una aventura inolvidable.',
+  }
+  {
+    question: '¿Cómo seleccionan los destinos para los paquetes de festivales de música?',
+    answer: 'Nuestro equipo de expertos busca los festivales más emocionantes y populares globalmente. Seleccionamos destinos que no solo ofrecen experiencias musicales excepcionales, sino también una mezcla única de cultura y entretenimiento.',
+  }
+  {
+    question: '¿Ltrip ofrece opciones de financiamiento o planes de pago para sus paquetes?',
+    answer: 'Sí, entendemos la importancia de hacer que los sueños de viaje sean accesibles. Ofrecemos opciones de financiamiento y planes de pago flexibles para adaptarnos a las necesidades financieras de nuestros clientes.',
+  }
+],
+placeId:""}
 
 
 /*
