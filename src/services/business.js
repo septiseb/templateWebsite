e044721a82cb6c1609caccc99e818a3dd3dc7009
Sebastian Septien <sebsepdus@hotmail.com>
2024-01-05 16:21:35 -0600

    changing the phone number of natscafe

--- src/services/business.js
+++ src/services/business.js
@@ -2,115 +2,127 @@ import { TrophyIcon } from '@heroicons/react/20/solid'
 
 export const business = {
   title: 'Nats Café',
-    description: 'En Nats Café, más que café, ofrecemos experiencias culinarias únicas. Desde desayunos revitalizantes hasta cenas excepcionales, fusionamos la pasión por el café con una cocina que deleitará tus sentidos. Y sí, horneamos nuestro propio pan a diario. Descubre un espacio acogedor diseñado para cada momento. Ya sea un rincón tranquilo para tu café matutino o un lugar animado para compartir, cada visita en Nats Café es especial. Explora nuestra propuesta culinaria. Reserva hoy y vive la magia de Nats Café. ¡Bienvenido a tu nuevo lugar favorito!',
-    keywords: 'Bebidas con y sin café, Bebidas calientes frías y frappe, Desayuno Almuerzo (Sandwiches & Bagels), Comidas, Panadería, Postres',
-    author: 'Nats Café',
-    email: 'natscafemx@gmail.com',
-    image: '',
-    url: 'https://natscafe.netlify.app/',
-    businessName: 'Nats Café',
-    address: {
-      streetAddress: 'Agustín Arroyo Chagoyan 205',
-      addressLocality: 'Celaya',
-      addressRegion: 'Gto',
-      postalCode: '38050',
-      addressCountry: 'Mexico',
-    },
-    phone: '524611143705',
-    geo: {
-      latitude: '20.529780402271417',   
-      longitude: '-100.80823239979533',
-    },
-    openingHours: 'Mo-Su 08:00-21:00',
-    socialLinks: [ 
-      'https://www.facebook.com/NatsCafeMx/',
-      'https://www.instagram.com/natscafemx/',
-      ],
-    testimonials: [{
+  description:
+    'En Nats Café, más que café, ofrecemos experiencias culinarias únicas. Desde desayunos revitalizantes hasta cenas excepcionales, fusionamos la pasión por el café con una cocina que deleitará tus sentidos. Y sí, horneamos nuestro propio pan a diario. Descubre un espacio acogedor diseñado para cada momento. Ya sea un rincón tranquilo para tu café matutino o un lugar animado para compartir, cada visita en Nats Café es especial. Explora nuestra propuesta culinaria. Reserva hoy y vive la magia de Nats Café. ¡Bienvenido a tu nuevo lugar favorito!',
+  keywords:
+    'Bebidas con y sin café, Bebidas calientes frías y frappe, Desayuno Almuerzo (Sandwiches & Bagels), Comidas, Panadería, Postres',
+  author: 'Nats Café',
+  email: 'natscafemx@gmail.com',
+  image: '',
+  url: 'https://natscafe.netlify.app/',
+  businessName: 'Nats Café',
+  address: {
+    streetAddress: 'Agustín Arroyo Chagoyan 205',
+    addressLocality: 'Celaya',
+    addressRegion: 'Gto',
+    postalCode: '38050',
+    addressCountry: 'Mexico',
+  },
+  phone: '524612969060',
+  geo: {
+    latitude: '20.529780402271417',
+    longitude: '-100.80823239979533',
+  },
+  openingHours: 'Mo-Su 08:00-21:00',
+  socialLinks: [
+    'https://www.facebook.com/NatsCafeMx/',
+    'https://www.instagram.com/natscafemx/',
+  ],
+  testimonials: [
+    {
       body: 'Este lugar se los recomiendo mil, Esta bueno el ambiente y su comida mil de diez',
       rating: 5,
       author: {
         name: 'Alexis Sepulveda',
       },
-       },{
+    },
+    {
       body: 'Me encantó sentarme afuerita del lugar , para sentir el rico aire y disfrutar mi bebida.',
       rating: 5,
       author: {
-        name: 'Socorro Villegas'
+        name: 'Socorro Villegas',
       },
-    },{
+    },
+    {
       body: 'Es una cafetería muy bonita y la atención es excelente,  el menú es variado y muy delicioso. Ideal para una cita de trabajo, lugar de estudio o para pasar un rato agradable rodeado de un ambiente urbano que recuerda a las películas.',
       rating: 5,
       author: {
         name: 'Eliceo Rodriguez',
       },
-    },{
+    },
+    {
       body: 'Este lugar se ha convertido en uno de mis favoritos, para desayunar en Celaya. La salsa de los chilaquiles es fabulosa. La atención al cliente ni se diga, el personal es muy amable y atento. Detalles como ofrecerte agua en tu llegada, me parece que le da un plus al lugar. En cuestión de panadería, les falta mejorar.',
       rating: 5,
       author: {
-        name: 'Mary Tere', 
+        name: 'Mary Tere',
       },
-    }],
-    features: [
-      {
-        name: 'Chilaquiles Nats',
-        description: 'Tomamos lo tradicional y lo hacemos a nuestra manera. La salsa, nuestro secreto de la casa, convierte los Chilaquiles Nats en una experiencia única. Descubre la fusión de sabores que solo encontrarás en Nats Café.',
-        icon: TrophyIcon,
-      },
-      {
-        name: 'Panadería Artesanal',
-        description: 'Cada pieza de pan se hornea con maestría en nuestra panadería. Deléitate con nuestras variedades de pan dulce y salado, donde la calidad y el sabor son nuestra especialidad. En Nats Café, cada bocado es una obra de arte culinaria.',
-        icon: TrophyIcon,
-      },
-      {
-        name: 'Servicio de Desayuno',
-        description: 'El desayuno es nuestro favorito. Disfruta de platillos hechos al momento, café delicioso y un servicio de calidad que te hará comenzar el día con energía y satisfacción. En Nats Café, cada desayuno es una experiencia para recordar.',
-        icon: TrophyIcon,
-      },
-      
-    ],
-    images:[{
-      src:"https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%208.34.29%20PM.jpeg",
-      description:"Nats Café en Celaya"
+    },
+  ],
+  features: [
+    {
+      name: 'Chilaquiles Nats',
+      description:
+        'Tomamos lo tradicional y lo hacemos a nuestra manera. La salsa, nuestro secreto de la casa, convierte los Chilaquiles Nats en una experiencia única. Descubre la fusión de sabores que solo encontrarás en Nats Café.',
+      icon: TrophyIcon,
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%208.34.30%20PM.jpeg",
-      description:"Nats Café en Celaya"
+      name: 'Panadería Artesanal',
+      description:
+        'Cada pieza de pan se hornea con maestría en nuestra panadería. Deléitate con nuestras variedades de pan dulce y salado, donde la calidad y el sabor son nuestra especialidad. En Nats Café, cada bocado es una obra de arte culinaria.',
+      icon: TrophyIcon,
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%209.39.44%20AM.jpeg",
-      description:"Nats Café en Celaya"
+      name: 'Servicio de Desayuno',
+      description:
+        'El desayuno es nuestro favorito. Disfruta de platillos hechos al momento, café delicioso y un servicio de calidad que te hará comenzar el día con energía y satisfacción. En Nats Café, cada desayuno es una experiencia para recordar.',
+      icon: TrophyIcon,
+    },
+  ],
+  images: [
+    {
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%208.34.29%20PM.jpeg',
+      description: 'Nats Café en Celaya',
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%209.39.45%20AM.jpeg",
-      description:"Nats Café en Celaya"
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%208.34.30%20PM.jpeg',
+      description: 'Nats Café en Celaya',
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%209.39.46%20AM.jpeg",
-      description:"Nats Café en Celaya"
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%209.39.44%20AM.jpeg',
+      description: 'Nats Café en Celaya',
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%209.39.47%20AM.jpeg",
-      description:"Nats Café en Celaya"
-    }
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%209.39.45%20AM.jpeg',
+      description: 'Nats Café en Celaya',
+    },
+    {
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%209.39.46%20AM.jpeg',
+      description: 'Nats Café en Celaya',
+    },
+    {
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/natscafe/WhatsApp%20Image%202023-11-25%20at%209.39.47%20AM.jpeg',
+      description: 'Nats Café en Celaya',
+    },
   ],
   faqs: [
-      {
-        question: '¿Qué hace a Nats Café diferente de otras cafeterías?',
-        answer: 'En Nats Café, nos destacamos por reinventar lo tradicional. Desde nuestros exclusivos Chilaquiles Nats hasta la variedad de panes horneados en nuestra panadería, cada detalle refleja nuestra pasión por ofrecer experiencias culinarias únicas y de alta calidad.',
-      },
-      {
-        question: '¿De dónde proviene la inspiración para los Chilaquiles Nats?',
-        answer: 'La inspiración para nuestros Chilaquiles Nats proviene de una combinación de tradición y creatividad. Nuestra salsa, un secreto de la casa, añade un toque único que transforma este platillo clásico en una experiencia gastronómica inolvidable.',
-      },
-      {
-        question: '¿Qué hace especial a la panadería de Nats Café?',
-        answer: 'En nuestra panadería, cada pieza de pan se hornea con maestría. Ya sea dulce o salado, nos esforzamos por ofrecer panes de alta calidad que complementen tu experiencia en Nats Café. Cada bocado es una deliciosa obra de arte culinaria.',
-      }
-    ],
-    placeId:"ChIJKWZCbvK7LIQRQuWqEtk--E0"
+    {
+      question: '¿Qué hace a Nats Café diferente de otras cafeterías?',
+      answer:
+        'En Nats Café, nos destacamos por reinventar lo tradicional. Desde nuestros exclusivos Chilaquiles Nats hasta la variedad de panes horneados en nuestra panadería, cada detalle refleja nuestra pasión por ofrecer experiencias culinarias únicas y de alta calidad.',
+    },
+    {
+      question: '¿De dónde proviene la inspiración para los Chilaquiles Nats?',
+      answer:
+        'La inspiración para nuestros Chilaquiles Nats proviene de una combinación de tradición y creatividad. Nuestra salsa, un secreto de la casa, añade un toque único que transforma este platillo clásico en una experiencia gastronómica inolvidable.',
+    },
+    {
+      question: '¿Qué hace especial a la panadería de Nats Café?',
+      answer:
+        'En nuestra panadería, cada pieza de pan se hornea con maestría. Ya sea dulce o salado, nos esforzamos por ofrecer panes de alta calidad que complementen tu experiencia en Nats Café. Cada bocado es una deliciosa obra de arte culinaria.',
+    },
+  ],
+  placeId: 'ChIJKWZCbvK7LIQRQuWqEtk--E0',
 }
-
 
 /*
 {
