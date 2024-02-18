313c088ded8c08db22ebfadcdd9d38f6647f4315
septiseb <sebsepdus@hotmail.com>
2024-02-18 10:24:19 -0600

    Adding the english version

--- src/services/business.js
+++ src/services/business.js
@@ -1,173 +1,105 @@
 import { TrophyIcon } from '@heroicons/react/20/solid'
 
 export const business = {
-  title: 'Muvin Fisioterapia',
-    description: 'Descubre nuestro consultorio especializado con más de 4 años de experiencia, dedicado a la rehabilitación, deporte, ortopedia y salud laboral. Transformamos vidas a través del movimiento. Recibe atención personalizada y experta para superar limitaciones, fortalecer tu cuerpo y alcanzar tu máximo potencial. ¡Bienvenido a una nueva era de salud y bienestar!',
-    keywords: 'Rehabilitación deportiva, rehabilitación ortopédica, rehabilitación laboral, Evaluación por ecografía musculoesqueletica',
-    author: 'Muvin Fisioterapia',
-    email: 'muvin.fisio@gmail.com',
-    image: '',
-    url: 'https://muvinfisioterapia.netlify.app/',
-    businessName: 'Muvin Fisioterapia',
-    address: {
-      streetAddress: 'Querétaro 125, Alameda',
-      addressLocality: 'Celaya',
-      addressRegion: 'Gto',
-      postalCode: '38050',
-      addressCountry: 'Mexico',
-    },
-    phone: '5214611796700',
-    geo: {
-      latitude: '20.532153724340372',    
-      longitude: '-100.804805780393',
-    },
-    openingHours: 'Mo-Fr 08:00-19:00, Sa 09:00-15:00',
-    socialLinks: [ 
-      'https://www.instagram.com/muvin.fisioterapia/',
-      '',
-      ],
-    testimonials: [{
-      body: 'Excelente atención y muy buena ubicación',
+  title: 'Dr. Yazmin Morales Implantology and Dental Aesthetics',
+  description:
+    'Discover the Perfect Smile with us! We offer diagnostics and preventive treatments, dental cleaning, digital smile design, digital impressions, and advanced treatments such as veneers, implants, metal-free restorations. Transform your smile today.',
+  keywords:
+    'Diagnosis and preventive treatments, Dental Cleaning, Digital Smile Design, Veneers, Implants, Metal-free restorations',
+  author: 'Dr. Yazmin Morales Implantology and Dental Aesthetics',
+  email: 'drayazmin.ms@hotmail.com',
+  image: '',
+  url: '',
+  businessName: 'Dr. Yazmin Morales Implantology and Dental Aesthetics',
+  address: {
+    streetAddress: 'Celaya Avenue, Plaza 4 Quince Local 5, C. San Miguel Km 3',
+    addressLocality: 'San Miguel de Allende',
+    addressRegion: 'Gto',
+    postalCode: '37700',
+    addressCountry: 'Mexico',
+  },
+  phone: '524151157121',
+  geo: {
+    latitude: '20.923211545520328',
+    longitude: '-100.757809097904',
+  },
+  openingHours: 'Mo-Fr 09:00-14:00, Mo-Fr 17:00-19:00, Sa 09:00-14:00',
+  socialLinks: ['https://www.facebook.com/drayazminmorales/', ''],
+  testimonials: [
+    {
+      body: 'I loved Dr. Yazmins attention, and best of all is how she improved my smile.',
       rating: 5,
       author: {
-        name: 'Alejandro',
+        name: 'Sebastian',
       },
-       },{
-      body: 'Gran atención, Te explican muy bien el problema por el que esta pasando tu cuerpo y se busca la mejor solución.',
+    },
+    {
+      body: 'Incredible attention from Dr. Yazmin. My smile has never looked better. Very grateful!',
       rating: 5,
       author: {
-        name: 'Ricardo'
+        name: 'Jorge M',
       },
-    },{
-      body: 'Excelente atención y las instalaciones están muy completas con una ubicación muy accesible y cuentan con estacionamiento',
+    },
+    {
+      body: 'Dr. Yazmin is an exceptional professional. Her attention made my dental experience wonderful. My smile is now my best accessory!',
       rating: 5,
       author: {
-        name: 'José Manuel Martínez Rosales',
+        name: 'Valentina',
       },
-    },{
-      body: 'Muy recomendable, buena atención por parte de Lau, siempre atenta. El lugar está muy bonito, limpio y seguro. Las descargas musculares son muy buenas, cuida cada detalle de tu sesión',
-      rating: 5,
-      author: {
-        name: 'Aidee Mendez Maldonado', 
-      },
-    }],
-    features: [
-      {
-        name: 'Electroterapia',
-        description: 'Utilizamos corrientes eléctricas controladas para estimular los músculos y promover la curación. Nuestra tecnología de vanguardia ofrece una experiencia segura y efectiva que puedes disfrutar desde la comodidad de tu hogar',
-        icon: TrophyIcon,
-      },
-      {
-        name: 'Ecografía Musculoesquelética',
-        description: 'Utilizamos esta herramienta avanzada que utiliza ondas de sonido para crear imágenes en tiempo real de los músculos, tendones, ligamentos y articulaciones. Esta tecnología nos permite obtener una comprensión sin igual de la anatomía, facilitando diagnósticos precisos y personalizados.',
-        icon: TrophyIcon,
-      },
-      {
-        name: 'Punción seca',
-        description: '¡Libera Tu Cuerpo del Dolor! Descubre la Punción Seca: Tu Solución Rápida y Personalizada para Aliviar Tensiones Musculares. ¡Agenda Ahora y Disfruta de la Libertad de un Cuerpo sin Limitaciones!',
-        icon: TrophyIcon,
-      },
-      
-    ],
-    images:[{
-      src:"https://storage.googleapis.com/web-assets-fluxsales/muvin/WhatsApp%20Image%202024-01-28%20at%206.43.10%20PM%20(3).jpeg",
-      description:"Muvin Fisioterapia"
+    },
+  ],
+  features: [
+    {
+      name: 'Digital Smile Design',
+      description:
+        'Enhance your smile with our digital preview. Before making changes in your mouth, visualize your treatment with confidence.',
+      icon: TrophyIcon,
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/muvin/WhatsApp%20Image%202024-01-28%20at%206.43.10%20PM%20(4).jpeg",
-      description:"Muvin Fisioterapia"
+      name: 'Veneers',
+      description:
+        'Transform the color and shape of your teeth with precise diagnosis and treatment planning. Achieve amazing results with minimal tooth wear.',
+      icon: TrophyIcon,
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/muvin/WhatsApp%20Image%202024-01-28%20at%206.43.10%20PM%20(5).jpeg",
-      description:"Muvin Fisioterapia"
+      name: 'Dental Implants',
+      description:
+        'Recover an area where you have lost one or more teeth permanently, without affecting adjacent teeth or resorting to removable prosthetics. Discover a uncomplicated dental rehabilitation solution.',
+      icon: TrophyIcon,
+    },
+  ],
+  images: [
+    {
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/yazmindentista/WhatsApp%20Image%202023-11-17%20at%203.27.36%20PM.jpeg',
+      description: 'Dr. Yazmin Morales Implantology and Dental Aesthetics',
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/muvin/WhatsApp%20Image%202024-01-28%20at%206.43.10%20PM.jpeg ",
-      description:"Muvin Fisioterapia"
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/yazmindentista/WhatsApp%20Image%202023-11-17%20at%203.45.40%20PM.jpeg',
+      description: 'Dr. Yazmin Morales Implantology and Dental Aesthetics',
     },
     {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/muvin/WhatsApp%20Image%202024-01-28%20at%206.43.11%20PM%20(1).jpeg",
-      description:"Muvin Fisioterapia"
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/yazmindentista/WhatsApp%20Image%202023-11-17%20at%206.33.08%20PM.jpeg',
+      description: 'Dr. Yazmin Morales Implantology and Dental Aesthetics',
     },
-    {
-      src:"https://storage.googleapis.com/web-assets-fluxsales/muvin/WhatsApp%20Image%202024-01-28%20at%206.43.11%20PM.jpeg",
-      description:"Muvin Fisioterapia"
-    }
   ],
   faqs: [
-      {
-        question: ' ¿En qué consiste la Electroterapia y cuáles son sus beneficios? ',
-        answer: 'La Electroterapia utiliza corrientes eléctricas controladas para estimular músculos y promover la curación. Beneficios incluyen alivio del dolor, recuperación muscular y mejora del rendimiento deportivo.',
-      },
-      {
-        question: '¿Cómo se realiza una Ecografía Musculoesquelética y por qué es beneficiosa? ',
-        answer: 'La Ecografía Musculoesquelética utiliza ondas de sonido para crear imágenes detalladas de músculos, tendones y articulaciones en tiempo real. Proporciona diagnósticos precisos, guía tratamientos y monitorea cambios durante movimientos.',
-      },
-      {
-        question: '¿Cuál es la diferencia entre la Punción Seca y otros tratamientos musculares? ',
-        answer: 'La Punción Seca utiliza agujas de acupuntura para tratar puntos gatillo miofasciales, ofreciendo alivio rápido y restauración de la movilidad. A diferencia de otros tratamientos, es personalizada y se centra en puntos específicos.',
-      }
-    ],
-    placeId:"ChIJi0pBH5y7LIQR65LhYs7e4bc"
-}
-
-
-/*
-{
-  title: "John's Pizza Palace",
-  description: 'The best pizza in town. Fresh ingredients and classic recipes.',
-  keywords: 'pizza, Italian cuisine, fast food, restaurant',
-  author: 'John Doe',
-  email:'contact@fluxsales.co',
-  image: 'https://example.com/images/pizza.jpg',
-  url: 'https://johnspizzapalace.com',
-  businessName: "John's Pizza Palace",
-  address: {
-    streetAddress: '123 Main St',
-    addressLocality: 'Springfield',
-    addressRegion: 'IL',
-    postalCode: '62701',
-    addressCountry: 'USA',
-  },
-  phone: '4611166531',
-  geo: {
-    latitude: '39.7817',
-    longitude: '89.6501',
-  },
-  openingHours: 'Mo,Tu,We,Th,Fr 09:00-17:00',
-  socialLinks: [
-    'https://facebook.com/johnspizzapalace',
-    'https://twitter.com/johnspizza',
+    {
+      question:
+        'What are the benefits of digital previewing in dental treatments?',
+      answer:
+        'Digital previewing allows patients to visualize how their smile will look after treatment, providing confidence and clarity before making changes in the mouth. Its a valuable tool for customizing and planning dental procedures.',
+    },
+    {
+      question: 'How does dental transformation with minimal tooth wear work?',
+      answer:
+        'Dental transformation with minimal wear involves using advanced techniques and technologies that preserve natural dental structure. This allows for aesthetic and functional changes without compromising the health of adjacent teeth.',
+    },
+    {
+      question:
+        'How does digital smile design differ in its approach to aesthetic improvement?',
+      answer:
+        'Digital smile design is an advanced tool we use to plan and customize your aesthetic treatment. It allows for precise visualization of desired results, providing you with a unique and participatory experience in the transformation of your smile.',
+    },
   ],
-  testimonials : [{
-    body: 'Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.',
-    rating:5,
-    author: {
-      name: 'Leslie Alexander',
-      handle: 'lesliealexander',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  }],
-  features:[
-    {
-      name: 'Push to deploy',
-      description:
-        'Commodo nec sagittis tortor mauris sed. Turpis tortor quis scelerisque diam id accumsan nullam tempus. Pulvinar etiam lacus volutpat eu. Phasellus praesent ligula sit faucibus.',
-      icon: TrophyIcon,
-    },
-    {
-      name: 'SSL certificates',
-      description:
-        'Pellentesque enim a commodProductoso malesuada turpis eleifend risus. Facilisis donec placerat sapien consequat tempor fermentum nibh.',
-      icon: TrophyIcon,
-    },
-    {
-      name: 'Simple queues',
-      description:
-        'Pellentesque sit elit congue ante nec amet. Dolor aenean curabitur viverra suspendisse iaculis eget. Nec mollis placerat ultricies euismod ut condimentum.',
-      icon: TrophyIcon,
-    },
-  ]
+  placeId: 'ChIJ_aZxFZpRK4QRKXB-zkOazf8',
 }
-*/
