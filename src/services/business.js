93a7768a6d1ab21ea41cd2a7f3117e18f8836642
septiseb <sebsepdus@hotmail.com>
2024-05-14 07:06:21 -0600

    Adding the landing with photos for 801

--- src/services/business.js
+++ src/services/business.js
@@ -1,136 +1,125 @@
 import { TrophyIcon } from '@heroicons/react/20/solid'
 
 export const business = {
-  title: 'Lorem Ipsum',
+  title: '801 Sports Bar',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae venenatis leo. Sed fermentum vestibulum justo, non lacinia nunc imperdiet at. Morbi et egestas tortor. Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Nulla facilisi.',
-  keywords:
-    'lorem, ipsum, dolor, sit, amet, consectetur, adipiscing, elit, vestibulum, praesent, morbi',
-  author: 'Lorem Ipsum - Dolor Sit Amet',
-  email: 'lorem@ipsum.com',
+    '¡Bienvenido a 801 sports bar en Celaya!  Disfruta de tus deportes favoritos en un ambiente vibrante y acogedor. Nuestro bar ofrece la combinación perfecta de emoción deportiva, buena compañía y sabores deliciosos. ¿Listo para vivir la experiencia definitiva de un sports bar? ¡Únete a nosotros y sumérgete en la pasión del juego!',
+  keywords: 'Sports bar, Fútbol, Alitas, Bebidas, Gastro Pub, Deportes',
+  author: '801 Sports Bar',
+  email: 'redes8001@gmail.com',
   image: '',
-  url: '',
-  businessName: 'Lorem Ipsum',
+  url: 'https://801Sportsbar.netlify.app/',
+  businessName: '801 Sports Bar',
   address: {
-    streetAddress: '123 Ipsum St.',
-    addressLocality: 'Dolor City',
-    addressRegion: 'Lorem Region',
-    postalCode: '12345',
-    addressCountry: 'Elitland',
+    streetAddress: 'Guillermo Prieto 801',
+    addressLocality: 'Celaya',
+    addressRegion: 'Gto',
+    postalCode: '38050',
+    addressCountry: 'Mexico',
   },
-  phone: '1234567890',
+  phone: '5214611363631',
   geo: {
-    latitude: '12.345678',
-    longitude: '98.765432',
+    latitude: '20.532555615493255',
+    longitude: '-100.80406019626221',
   },
-  openingHours: "",
-  socialLinks: ["https://www.instagram.com/loremipsum/",""],
+  openingHours: 'Mo-Tu 13:00-00:00, We-Sa 13:00-02:00, Su 13:00-22:00',
+  socialLinks: ['https://www.instagram.com/801.bar/', ''],
   testimonials: [
     {
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed lacinia urna, eget imperdiet mauris. Morbi a ante ac lacus fringilla tincidunt at eu nunc. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+      body: 'Un lugar para disfrutar partidos de fútbol y todos los deportes con tus amigos. excelente música y sonido. Todo es ampliamente recomendado y seguro. Sin duda uno de los mejores lugares en celaya.',
       rating: 5,
       author: {
-        name: 'John Doe',
+        name: 'Fabian',
       },
     },
     {
-      body: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      body: 'El mejor bar de Celaya para convivir :yum:',
       rating: 5,
       author: {
-        name: 'Jane Smith',
+        name: 'Dani',
       },
     },
     {
-      body: 'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada. Curabitur vel dui eu augue cursus luctus a non risus. Sed nec sapien quis turpis facilisis congue. Etiam ac neque eu lorem volutpat rhoncus. Maecenas tincidunt, orci ut efficitur eleifend, urna ligula molestie ex, ac porttitor diam urna at lacus.',
+      body: 'Excelente lugar y excelente atención, buena musica con el DJ Oscar, muy recomendable',
       rating: 5,
       author: {
-        name: 'James Bond',
+        name: 'Luis',
       },
     },
     {
-      body: 'Sed fermentum vestibulum justo, non lacinia nunc imperdiet at. Nullam a est elit. Integer id mauris nec orci efficitur bibendum non a magna. Vivamus in mollis purus. In vulputate, magna eget vehicula finibus, odio metus ultricies dolor, ac sollicitudin elit orci a dui.',
+      body: '¡Me encanta! Mi lugar favorito para ver deportes y comer muy rico. Buen ambiente y el servicio es muy rápido',
       rating: 5,
       author: {
-        name: 'Lucy Heartfilia',
+        name: 'Natalia',
       },
     },
   ],
   features: [
     {
-      name: 'Lorem Feature 1',
+      name: 'Alimentos',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et egestas tortor.',
+        'Deléitate con una variedad de platillos cuidadosamente seleccionados que podrás disfrutar con familia y amigos. Desde clásicos favoritos hasta creaciones únicas, nuestra cocina está lista para satisfacer todos los gustos.',
       icon: TrophyIcon,
     },
     {
-      name: 'Lorem Feature 2',
+      name: 'Bebidas',
       description:
-        'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada.',
+        'Sumérgete en un mundo de sabores con nuestras exquisitas opciones en mixología. Desde cócteles clásicos hasta creaciones innovadoras, cada sorbo es una experiencia que encantará tu paladar y te dejará con ganas de más.',
       icon: TrophyIcon,
     },
     {
-      name: 'Lorem Feature 3',
+      name: 'Ambiente',
       description:
-        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;',
+        'Experimenta la combinación perfecta de nuestra cocina y nuestras bebidas, que harán de tu visita una experiencia inolvidable. Con un ambiente acogedor y una energía vibrante, te garantizamos que cada momento en nuestro sports bar será único y te dejará con ganas de regresar.',
       icon: TrophyIcon,
     },
   ],
   images: [
     {
-      src: 'https://www.example.com/image1.jpeg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/barochocientos/Screenshot%202024-05-14%20at%207.02.16%E2%80%AFAM.png',
+      description: '801 Sports Bar',
     },
     {
-      src: 'https://www.example.com/image2.jpeg',
-      description: 'Praesent vitae venenatis leo',
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/barochocientos/Screenshot%202024-05-14%20at%207.02.44%E2%80%AFAM.png',
+      description: '801 Sports Bar',
     },
     {
-      src: 'https://www.example.com/image3.jpeg',
-      description: 'Morbi et egestas tortor',
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/barochocientos/Screenshot%202024-05-14%20at%207.02.52%E2%80%AFAM.png',
+      description: '801 Sports Bar',
     },
     {
-      src: 'https://www.example.com/image4.jpeg',
-      description: 'Sed fermentum vestibulum justo',
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/barochocientos/Screenshot%202024-05-14%20at%207.03.06%E2%80%AFAM.png',
+      description: '801 Sports Bar',
     },
     {
-      src: 'https://www.example.com/image5.jpeg',
-      description: 'Vivamus euismod purus nec ligula hendrerit',
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/barochocientos/Screenshot%202024-05-14%20at%207.03.18%E2%80%AFAM.png',
+      description: '801 Sports Bar',
     },
     {
-      src: 'https://www.example.com/image6.jpeg',
-      description: 'Vestibulum ante ipsum primis in faucibus',
-    }
+      src: 'https://storage.googleapis.com/web-assets-fluxsales/barochocientos/Screenshot%202024-05-14%20at%207.03.29%E2%80%AFAM.png',
+      description: '801 Sports Bar',
+    },
   ],
   faqs: [
     {
-      question: 'Lorem ipsum dolor sit amet?',
+      question: ' ¿Tienen pantallas para ver varios juegos simultáneamente? ',
       answer:
-        'Consectetur adipiscing elit. Vivamus euismod purus nec ligula hendrerit.',
+        'Sí, contamos con múltiples pantallas HD estratégicamente ubicadas para que puedas disfrutar de varios juegos al mismo tiempo y no te pierdas ni un solo momento de la acción.',
     },
     {
-      question: 'Praesent vitae venenatis leo?',
+      question:
+        '¿Puedo reservar una mesa para un grupo grande o una celebración especial? ',
       answer:
-        'Morbi et egestas tortor. Sed fermentum vestibulum justo.',
+        'Sí, aceptamos reservas para grupos grandes y ocasiones especiales. Ponte en contacto con nosotros con anticipación y estaremos encantados de ayudarte a organizar tu evento.',
     },
     {
-      question: 'Vivamus euismod purus nec ligula?',
+      question: '¿Organizan eventos especiales durante los juegos importantes?',
       answer:
-        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;',
-    },
-    {
-      question: 'Sed fermentum vestibulum justo?',
-      answer:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et egestas tortor.',
-    },
-    {
-      question: 'Morbi et egestas tortor?',
-      answer:
-        'Vivamus euismod purus nec ligula hendrerit, at fermentum neque malesuada.',
+        '¡Por supuesto! Organizamos eventos especiales para los juegos más importantes, como finales de campeonatos, partidos de selecciones nacionales y otros eventos deportivos destacados. Mantente atento a nuestras redes sociales y sitio web para conocer más detalles.',
     },
   ],
-  placeId: 'LoremIpsumPlaceId12345',
+  placeId: 'ChIJtYSfl4K7LIQRdSJvVFybmnc',
 }
-
 
 /*
 {
